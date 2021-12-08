8b7d2d2368eb441e82a9d3bb7d232add37f1d573
0x41gawor <41gawor@gmail.com>
2021-12-08 02:25:48 +0100

    chore: create Android Studio project #1

---
+++ app/src/main/java/pl/gawor/android/tayckner/MainActivity.kt
@@ -0,0 +1,11 @@
+package pl.gawor.android.tayckner
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
