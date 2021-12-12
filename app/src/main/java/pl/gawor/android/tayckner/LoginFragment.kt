fb4ee633918b39192d3158e441e5fca1644c8670
0x41gawor <41gawor@gmail.com>
2021-12-12 21:32:31 +0100

    fix(login): implement layout buttons #5

--- app/src/main/java/pl/gawor/android/tayckner/LoginFragment.kt
+++ app/src/main/java/pl/gawor/android/tayckner/LoginFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -34,7 +37,13 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        val view = inflater.inflate(R.layout.fragment_login, container, false)
+        val buttonAction = view.findViewById<Button>(R.id.button_action)
+        val buttonSwitch = view.findViewById<Button>(R.id.button_switch)
+
+        buttonAction.setOnClickListener { Toast.makeText(context, "Not implemented yet", Toast.LENGTH_SHORT).show() }
+        buttonSwitch.setOnClickListener { findNavController().navigate(R.id.registerFragment) }
+        return view
     }
 
     companion object {
