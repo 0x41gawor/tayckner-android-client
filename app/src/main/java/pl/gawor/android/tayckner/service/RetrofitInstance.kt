f246bc9d29e9a7ebc7a9b70a5bdb2d20506cee82
0x41gawor <41gawor@gmail.com>
2021-12-14 22:00:12 +0100

    feat(habit-tracker-service): add second retrofit instance for habits url #16

--- app/src/main/java/pl/gawor/android/tayckner/service/RetrofitInstance.kt
+++ app/src/main/java/pl/gawor/android/tayckner/service/RetrofitInstance.kt
@@ -5,10 +5,18 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInstance {
 
-    val retrofit: Retrofit by lazy {
+    val retrofitUsers: Retrofit by lazy {
         Retrofit.Builder()
-            .baseUrl("http://10.0.2.2:8080/api/users/")
-//            .baseUrl("http://192.168.1.149:8080/api/users/")
+//            .baseUrl("http://10.0.2.2:8080/api/users/")
+            .baseUrl("http://192.168.1.149:8080/api/users/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    val retrofitHabits: Retrofit by lazy {
+        Retrofit.Builder()
+//            .baseUrl("http://10.0.2.2:8080/api/habits/")
+            .baseUrl("http://192.168.1.149:8080/api/habits/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
