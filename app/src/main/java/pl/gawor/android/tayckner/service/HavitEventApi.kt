6209354b180f5a79c190edd6f481dbafab3207d9
0x41gawor <41gawor@gmail.com>
2021-12-16 20:46:32 +0100

    feat(habit-tracker-service): define habit-event api #16

---
+++ app/src/main/java/pl/gawor/android/tayckner/service/HavitEventApi.kt
@@ -0,0 +1,12 @@
+package pl.gawor.android.tayckner.service
+
+import pl.gawor.android.tayckner.model.HabitEvent
+import pl.gawor.android.tayckner.model.ResponseModel
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Header
+
+interface HavitEventApi {
+    @GET("habits")
+    suspend fun list(@Header("Authorization") auth: String): Response<ResponseModel<List<HabitEvent>>>
+}
