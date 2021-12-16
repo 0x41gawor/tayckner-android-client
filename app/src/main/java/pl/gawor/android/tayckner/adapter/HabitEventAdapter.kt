4e3d0835053e3c95b9769df7e52a90bee21b6105
0x41gawor <41gawor@gmail.com>
2021-12-16 20:59:55 +0100

    fix(habit-tracker-model): fix habit model class #16

---
+++ app/src/main/java/pl/gawor/android/tayckner/adapter/HabitEventAdapter.kt
@@ -0,0 +1,53 @@
+package pl.gawor.android.tayckner.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import pl.gawor.android.tayckner.databinding.HabitEventItemBinding
+import pl.gawor.android.tayckner.model.HabitEvent
+
+class HabitEventAdapter : RecyclerView.Adapter<HabitEventAdapter.HabitEventViewHolder>()  {
+    inner class HabitEventViewHolder(val binding: HabitEventItemBinding) : RecyclerView.ViewHolder(binding.root)
+
+    private val diffCallback = object : DiffUtil.ItemCallback<HabitEvent>() {
+        override fun areItemsTheSame(oldItem: HabitEvent, newItem: HabitEvent): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: HabitEvent, newItem: HabitEvent): Boolean {
+            return oldItem == newItem
+        }
+
+    }
+
+    private val differ = AsyncListDiffer(this, diffCallback)
+
+    var habitEvents: List<HabitEvent>
+        get() = differ.currentList
+        set(value) {differ.submitList(value)}
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HabitEventViewHolder {
+       return HabitEventViewHolder(HabitEventItemBinding.inflate(
+           LayoutInflater.from(parent.context),
+           parent,
+           false
+       ))
+    }
+
+    override fun onBindViewHolder(holder: HabitEventViewHolder, position: Int) {
+        holder.binding.apply {
+            val habitEvent = habitEvents[position]
+            //TODO
+            //textViewHabitName.text = habitEvent.
+        }
+    }
+
+    override fun getItemCount(): Int {
+       return habitEvents.size
+    }
+
+
+}
--- app/src/main/java/pl/gawor/android/tayckner/model/HabitEvent.kt
+++ app/src/main/java/pl/gawor/android/tayckner/model/HabitEvent.kt
@@ -1,11 +1,9 @@
 package pl.gawor.android.tayckner.model
 
 data class HabitEvent(
-    val color: String,
     val comment: String,
     val date: String,
+    val habit: Habit,
     val id: Int,
-    val name: String,
-    val user: User,
     val value: Int
 )
