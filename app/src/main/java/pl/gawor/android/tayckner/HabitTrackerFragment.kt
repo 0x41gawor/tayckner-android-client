ab3b2070154c2289d9cde48ac77fa98964347a8f
0x41gawor <41gawor@gmail.com>
2021-12-21 17:16:57 +0100

    feat(habit-tracker-layout): add add habit event layout #16

--- app/src/main/java/pl/gawor/android/tayckner/HabitTrackerFragment.kt
+++ app/src/main/java/pl/gawor/android/tayckner/HabitTrackerFragment.kt
@@ -1,5 +1,6 @@
 package pl.gawor.android.tayckner
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -12,6 +13,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import pl.gawor.android.tayckner.adapter.HabitAdapter
 import pl.gawor.android.tayckner.adapter.HabitEventAdapter
 import pl.gawor.android.tayckner.databinding.FragmentHabitTrackerBinding
+import pl.gawor.android.tayckner.databinding.ItemAddHabitEventBinding
 import pl.gawor.android.tayckner.model.Habit
 import pl.gawor.android.tayckner.model.HabitEvent
 import pl.gawor.android.tayckner.model.ResponseModel
@@ -23,7 +25,7 @@ import retrofit2.Response
 import java.io.IOException
 
 
-const val JWT_TOKEN = "Bearer " + "eyJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2Mzk2OTM4MjYsImV4cCI6MTYzOTcwMzM0OCwidXNlcklkIjozLCJ1c2VybmFtZSI6IndhemEifQ.fGz7Xd4rc8udNan8yqn12bweh4Bg8r3GHQn3HAmr8O8"
+const val JWT_TOKEN = "Bearer " + "eyJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2NDAxMDEyOTIsImV4cCI6MTY0MDExMDgxNCwidXNlcklkIjozLCJ1c2VybmFtZSI6IndhemEifQ.Av4MovhJ1VPrwk4V0TMqlLT4BchDQRktb67PL5DljRw"
 
 class HabitTrackerFragment : Fragment() {
 
@@ -58,7 +60,7 @@ class HabitTrackerFragment : Fragment() {
             Toast.makeText(context, "Habit-tracker button not implemented yet", Toast.LENGTH_SHORT).show()
         }
         binding.imageButtonAdd.setOnClickListener {
-            Toast.makeText(context, "Add habit-event button not implemented yet", Toast.LENGTH_SHORT).show()
+            addHabitEvent()
         }
 
         return binding.root
@@ -120,6 +122,33 @@ class HabitTrackerFragment : Fragment() {
                 Log.e(TAG, "HabitTrackerFragment.sendHabitEventsListRequest: HTTP status != 200")
             }
         }
+    }
+
+
+    private fun addHabitEvent() {
+       val bindingAddHabitEvent = ItemAddHabitEventBinding.inflate(layoutInflater)
+
+        val editTextHabitId = bindingAddHabitEvent.editTextHabit
+        val editTextDate = bindingAddHabitEvent.editTextDate
+        val editTextComment = bindingAddHabitEvent.editTextDate
+        val editTextValue = bindingAddHabitEvent.editTextValue
+
+        val dialogAddHabitEvent = AlertDialog.Builder(context)
+
+        dialogAddHabitEvent.setView(bindingAddHabitEvent.root)
+
+        dialogAddHabitEvent.setPositiveButton("OK") {
+                dialog,_->
+            Toast.makeText(context, "Not implemented yet", Toast.LENGTH_SHORT).show()
+            dialog.dismiss()
+        }
+        dialogAddHabitEvent.setNegativeButton("Cancel") {
+                dialog,_->
+            Toast.makeText(context, "Adding canceled", Toast.LENGTH_SHORT).show()
+            dialog.dismiss()
+        }
+        dialogAddHabitEvent.create()
+        dialogAddHabitEvent.show()
     }
 
     companion object {
