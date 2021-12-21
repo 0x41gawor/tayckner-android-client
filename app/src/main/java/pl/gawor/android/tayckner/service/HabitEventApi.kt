b4fdac6591659ef38f1543b3969be5de08bad679
0x41gawor <41gawor@gmail.com>
2021-12-21 18:43:53 +0100

    feat(habit-tracker-service): add habit-events create endpoint #16

--- app/src/main/java/pl/gawor/android/tayckner/service/HabitEventApi.kt
+++ app/src/main/java/pl/gawor/android/tayckner/service/HabitEventApi.kt
@@ -3,10 +3,15 @@ package pl.gawor.android.tayckner.service
 import pl.gawor.android.tayckner.model.HabitEvent
 import pl.gawor.android.tayckner.model.ResponseModel
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.POST
 
 interface HabitEventApi {
     @GET("habit-events")
     suspend fun list(@Header("Authorization") auth: String): Response<ResponseModel<List<HabitEvent>>>
+
+    @POST("habit-events")
+    suspend fun create(@Header("Authorization") auth: String, @Body habitEvent: HabitEvent): Response<ResponseModel<HabitEvent>>
 }
