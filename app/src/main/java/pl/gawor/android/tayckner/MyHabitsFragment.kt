c5b18a87be7da82350b24d3180b587cd30c16603
0x41gawor <41gawor@gmail.com>
2021-12-22 19:23:39 +0100

    feat(habit-tracker-fragment): implement my habits refresh list call #20

--- app/src/main/java/pl/gawor/android/tayckner/MyHabitsFragment.kt
+++ app/src/main/java/pl/gawor/android/tayckner/MyHabitsFragment.kt
@@ -1,22 +1,31 @@
 package pl.gawor.android.tayckner
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import pl.gawor.android.tayckner.adapter.MyHabitAdapter
 import pl.gawor.android.tayckner.databinding.FragmentMyHabitsBinding
+import pl.gawor.android.tayckner.model.Habit
+import pl.gawor.android.tayckner.repository.HabitRepository
 
 class MyHabitsFragment : Fragment() {
+
+    private  val TAG = "TAYCKNER"
 
     private lateinit var binding: FragmentMyHabitsBinding
 
     private lateinit var habitAdapter: MyHabitAdapter
 
+    private val repository = Repository()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        repository.refreshHabitsList()
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -28,9 +37,20 @@ class MyHabitsFragment : Fragment() {
     private fun setupHabitRecyclerView()  = binding.recyclerViewMyHabits.apply {
         habitAdapter = MyHabitAdapter()
         adapter = habitAdapter
-        layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+        layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
     }
 
+    inner class Repository{
+        fun refreshHabitsList() {
+            Log.i(TAG, "MyHabitsFragment.Repository.refreshHabitsList() = void")
+            lifecycleScope.launchWhenCreated {
+                val list :List<Habit> = HabitRepository.list()
+                habitAdapter.habits = list
+                return@launchWhenCreated
+            }
+            Log.i(TAG, "MyHabitsFragment.Repository.refreshHabitsList() = void")
+        }
+    }
 
     companion object {
         @JvmStatic
