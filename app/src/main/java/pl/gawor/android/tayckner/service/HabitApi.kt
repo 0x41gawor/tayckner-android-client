4137661f1c7ebe1a6c76c6ed30c7fe4f6ce062cd
0x41gawor <41gawor@gmail.com>
2021-12-27 20:25:22 +0100

    feat(habit-tracker-service): add habit endpoints for update and delete #22

--- app/src/main/java/pl/gawor/android/tayckner/service/HabitApi.kt
+++ app/src/main/java/pl/gawor/android/tayckner/service/HabitApi.kt
@@ -3,10 +3,7 @@ package pl.gawor.android.tayckner.service
 import pl.gawor.android.tayckner.model.Habit
 import pl.gawor.android.tayckner.model.ResponseModel
 import retrofit2.Response
-import retrofit2.http.Body
-import retrofit2.http.GET
-import retrofit2.http.Header
-import retrofit2.http.POST
+import retrofit2.http.*
 
 interface HabitApi {
 
@@ -15,4 +12,10 @@ interface HabitApi {
 
     @POST("habits")
     suspend fun create(@Header("Authorization") auth: String, @Body habit: Habit): Response<ResponseModel<Habit>>
+
+    @PUT("habits/{id}")
+    suspend fun update(@Header("Authorization") auth: String, @Body habit: Habit, @Path("id") id: Int): Response<ResponseModel<Habit>>
+
+    @DELETE("habits/{id}")
+    suspend fun delete(@Header("Authorization") auth: String, @Path("id") id: Int): Response<ResponseModel<Any>>
 }
